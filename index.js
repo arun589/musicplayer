0b9a7b55e690ca139ee3949c06208cd288f999eb
arun589 <83627958+arun589@users.noreply.github.com>
2023-05-24 12:57:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ for(var i=0;i<noOfColor;i++)
        switch(b)
        {
         case 'w':
-            new Audio("./sounds/crash.mp3").play();
+            new Audio("crash.mp3").play();
             break;
         case 'a':
-            new Audio("./sounds/kick-bass.mp3").play();
+            new Audio("kick-bass.mp3").play();
             break;
         case 's':
-            new Audio("./sounds/snare.mp3").play();
+            new Audio("snare.mp3").play();
             break;
         case 'd':
-                new Audio("./sounds/tom-1.mp3").play();
+                new Audio("tom-1.mp3").play();
                 break;
         case 'j':
-                    new Audio("./sounds/tom-2.mp3").play();
+                    new Audio("tom-2.mp3").play();
                     break;
         case "k":
-            new Audio("./sounds/tom-3.mp3").play();
+            new Audio("tom-3.mp3").play();
             break;
         case 'l':
-                new Audio("./sounds/tom-4.mp3").play();
+                new Audio("tom-4.mp3").play();
                 break;
        }
     })
